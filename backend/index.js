44e9eb6a5de2d1666ae0a32a70b4e31e71988b48
Soumwadeep Guha <soumwadeepguha@Soumwadeeps-Mac-mini.local>
2023-07-13 12:44:17 +0530

    Added Domain

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: [""],
+    origin: ["https://make-u-perfect.vercel.app/"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
