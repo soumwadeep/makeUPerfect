aa6fc17fbe25ea7212d315d7bd937f6d7d9e161d
Soumwadeep Guha <soumwadeepguha@Soumwadeeps-Mac-mini.local>
2023-10-25 10:26:15 +0530

    Addeed netlify domain

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,7 @@ app.use(
       "http://localhost:3000",
       "http://task.makememaster.in",
       "https://task.makememaster.in",
+      "https://makeuperfectmain.netlify.app/"
     ],
     credentials: true,
   })
